80b74ac49c378aa59dfe4c77e39e597707de2659
robmarkcole <robmarkcole@gmail.com>
2017-12-29 17:06:43 +0000

    Update with password

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # HASS-hammerspoon
 [hammerspoon](http://www.hammerspoon.org/) init.lua script to toggle a Home-assistant switch when my MacBook wakes/sleeps.
+Makes use of [hs.http.post](http://www.hammerspoon.org/docs/hs.http.html#post).
+Just **brew install hammerspoon**, navigate to **~/.hammerspoon** and add init.lua.
+If you don't use a password on your HASS instance, just delete **headers** from init.lua.
 
-In my home-assistant config I simply have:
+In my home-assistant I use a dummy command_line switch to represent the Mac, and in my config I simply have:
 ```
 switch:
   - platform: command_line
--- init.lua
+++ init.lua
@@ -1,12 +1,16 @@
 data = '{"entity_id": "switch.macbook_power"}'
 
+headers = {}
+headers["x-ha-access"] = "your_password"
+
+
 local sleepWatcher = hs.caffeinate.watcher.new(function (eventType)
       if (eventType == hs.caffeinate.watcher.systemDidWake) then
           hs.notify.show("Wake sequence initiated!", "", "")
-     		  hs.http.post("http://192.168.0.100:8123/api/services/switch/turn_on", data)
+     		  hs.http.post("http://192.168.0.100:8123/api/services/switch/turn_on", data, headers)
       elseif (eventType == hs.caffeinate.watcher.systemWillSleep) then
             hs.notify.show("Bye!", "", "")
-     		    hs.http.post("http://192.168.0.100:8123/api/services/switch/turn_off", data)
+     		    hs.http.post("http://192.168.0.100:8123/api/services/switch/turn_off", data, headers)
       end
 end)
 sleepWatcher:start()
