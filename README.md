1d6aba8b3f52c56a5552f0832d5164530350b024
robmarkcole <robmarkcole@gmail.com>
2017-12-29 13:42:12 +0000

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # HASS-hammerspoon
-hammerspoon script to toggle a Home-assistant switch on wake/sleep of my MacBook
+[hammerspoon](http://www.hammerspoon.org/) init.lua script to toggle a Home-assistant switch when my MacBook wakes/sleeps.
+
+In my home-assistant config I simply have:
+```
+switch:
+  - platform: command_line
+    switches:
+      macbook_power:
+        command_on: ""
+        command_off: ""
+```
---
+++ init.lua
@@ -0,0 +1,12 @@
+data = '{"entity_id": "switch.macbook_power"}'
+
+local sleepWatcher = hs.caffeinate.watcher.new(function (eventType)
+      if (eventType == hs.caffeinate.watcher.systemDidWake) then
+          hs.notify.show("Wake sequence initiated!", "", "")
+     		  hs.http.post("http://192.168.0.100:8123/api/services/switch/turn_on", data)
+      elseif (eventType == hs.caffeinate.watcher.systemWillSleep) then
+            hs.notify.show("Bye!", "", "")
+     		    hs.http.post("http://192.168.0.100:8123/api/services/switch/turn_off", data)
+      end
+end)
+sleepWatcher:start()
